39bb38917fb72e1cb810e88b5732313bc5223962
MarioCaushi <caushimario321@gmail.com>
2025-01-02 01:35:50 +0100

    Completed View All Employees Page

--- src/Components/EmployeeCard.jsx
+++ src/Components/EmployeeCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 
-function EmployeeCard() {
+function EmployeeCard({employee, handleDeleteButton}) {
     return (
         <div className="col-lg-3 col-md-4 col-sm-6 m-3"> {/* Include margin-bottom for spacing between rows */}
             <div className="d-flex flex-column align-items-center p-3 bg-white border shadow rounded">
                 <div className="text-center">
-                    <p className="employee-name m-0"><strong>Mario Caushi</strong></p>
-                    <p className="employee-position m-0 text-muted">Developer</p>
+                    <p className="employee-name m-0"><strong>{employee.name} {employee.lastName}</strong></p>
+                    <p className="employee-position m-0 text-muted">{employee.position}</p>
                 </div>
                 <div className="employee-info mt-2 text-center">
-                    <p className="m-0">Status: Active</p>
-                    <p className="m-0">Type: Full Time</p>
-                    <p className="m-0"><strong> ID: f</strong></p>
+                    <p className="m-0">Status: {employee.status}</p>
+                    <p className="m-0">Type: {employee.type}</p>
+                    <p className="m-0"><strong> ID: {employee.employeeId}</strong></p>
                 </div>
                 <div className="employee-actions mt-2">
                     <button type="button" className="btn btn-info btn-sm m-1 rounded-2" id="view-f">View Details</button>
                     <button type="button" className="btn btn-warning btn-sm m-1 rounded-2" id="edit-f">Edit</button>
-                    <button type="button" className="btn btn-danger btn-sm m-1 rounded-2" id="delete-f">Delete</button>
+                    <button type="button" className="btn btn-danger btn-sm m-1 rounded-2" id="delete-f" onClick={handleDeleteButton}>Delete</button>
                 </div>
             </div>
         </div>
